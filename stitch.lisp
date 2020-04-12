5a1920328245a4c3d82fc6a0ae5d46e0e473b92b
Abhinav Tushar <abhinav@lepisma.xyz>
2020-04-12 14:11:27 +0530

    few dummy fns

--- stitch.lisp
+++ stitch.lisp
@@ -1,5 +1,7 @@
 (in-package #:stitch)
 
+;; Label reading
+
 (defstruct audio-resource
   "An audio resource represents slicing information and filepath for various
 symbols present in the file."
@@ -25,3 +27,29 @@ labels is in order."
                    (split #\newline)
                    (remove-if (lambda (line) (zerop (length line))) <>))))
     (make-audio-resource :values (mapcar #'parse-label-line lines) :filepath filepath)))
+
+;; Wav interaction
+
+(defun slice-wav-data (data start-time end-time)
+  "Slice wav data (read from cl-wav) from start-time to end-time.")
+
+(defun merge-wav-data (data-a data-b)
+  "Merge two wav data pieces in a one data piece.")
+
+(defun swap-extension (filepath new-ext)
+  "Primitive extension swapping for files with single extension component."
+  (let ((splits (split filepath ".")))
+    (join (append (butlast splits) (list new-ext)) :separator ".")))
+
+(defun read-wav-for-labels-file (label-filepath)
+  "Read wav file corresponding to the label file. We assume that that wav file
+has same name as the labels file with extension changed."
+  (wav:read-wav-file (swap-extension label-filepath "wav")))
+
+;; Planning
+
+(defun find-plan (sequence audio-resources)
+  "For the given list of sequence of symbols, look up audio resources.")
+
+(defun stitch-plan (plan output-filepath)
+  "Take the list of audio-resources stitch the final audio output and put in.")
